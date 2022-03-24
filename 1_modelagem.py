a2014772c9391d510a5d030116ce99684859f641
alefenba <alefe.182@outlook.com>
2022-03-24 20:22:09 -0300

    Entendendo como funciona o método construtor __init__ #2

--- 1_modelagem.py
+++ 1_modelagem.py
@@ -1,18 +1,13 @@
-# modelagem da Classe
+
 class Pessoas:
     def __init__(self, nome, idade, cpf):
-        self.nome = nome
-        self.idade = idade
-        self.cpf = cpf
+        print(f'{nome} | {idade} | {cpf}')
+        
 
-    #método
     def logar_sistema(self):
-        print(f'{self.nome}, seu login foi Bem Sucedido!')
+        print('Estou logando no sistema!')
+
+#Sempre que criado uma nova instancia o método construtor será executado
+pessoa_1 = Pessoas('Alefe Gomes', 24, '12345678978')
 
 
-#Criando instancias
-pessoa_1 = Pessoas('Alefe Gomes', 24, '12345678900')
-pessoa_2 = Pessoas('Marcos Antonio', 25, '12378945625')
-
-pessoa_1.logar_sistema()
-pessoa_2.logar_sistema() 
