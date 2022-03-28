95f860cad6ba646f8b3f273179d74822cefbc78b
alefenba <alefe.182@outlook.com>
2022-03-28 20:17:28 -0300

    @staticmethod, um método utilitario  #7

--- 1_modelagem.py
+++ 1_modelagem.py
@@ -1,5 +1,5 @@
 '''
-CLS é um parametro a qual recebe o estado da classe 
+Staticmethod é apenas um método utilitario , e não possui informações da sua classe
 '''
 
 class Pessoas:
@@ -22,10 +22,16 @@ class Pessoas:
         cls.possui_boca = False
         return None
 
+    @staticmethod
+    def e_adulto(idade):
+        if idade > 18 : 
+            return True
+        return False 
+        
+
 #p1 = Pessoas('Alefe Gomes', 24)
 #p2 = Pessoas('Caio sampaio', 21)
 
-print(Pessoas.possui_boca)
-Pessoas.andar()
-print (Pessoas.possui_boca)
+print(Pessoas.e_adulto(24))
+
 
